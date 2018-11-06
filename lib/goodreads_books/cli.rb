78890eaec600a65943db59d58b2fcf15cccc666f
Travis Williamson <travywill@gmail.com>
2018-11-06 12:35:59 +0000

    Creates conduct_search method

--- lib/goodreads_books/cli.rb
+++ lib/goodreads_books/cli.rb
@@ -14,7 +14,8 @@ class CLI # This class runs the command-line interface for the gem
     puts "Please enter your key words to search for a book: "
     query = gets.strip
     puts ""
-    list_books(query) # Runs the list_books method with the user's key words to print a list of books to the screen
+    conduct_search(query) # Runs the conduct_search method with the user's key words to create an array of Book objects
+    list_books # Runs the list_books method to print a list of the books from the array to the screen
     puts ""
     puts "Would you like to read a summary of one of these books? (y/n)"
     input = gets.strip
@@ -43,9 +44,13 @@ class CLI # This class runs the command-line interface for the gem
     end
   end
 
-  def list_books(query)
+  def conduct_search(query)
+    # This method creates an array of Book objects based on the user's search terms
+    @book_list = Scraper.scrape_search_results(query)
+  end
+
+  def list_books
     # This method prints a numbered list of books to the screen based on the user's search terms
-    @book_list = Scraper.scrape_search_results(query) # Creates an array of Book objects based on user's search terms
     @book_list.each_with_index do |book, index| # Prints a numbered list of the Books to the screen
       if book.authors.length == 1 # Formatting for book with one author
         puts "#{index + 1}. #{book.title} by #{book.authors[0]}"
@@ -74,9 +79,10 @@ class CLI # This class runs the command-line interface for the gem
       input = gets.strip
       input = input.to_i
     end
-    Scraper.scrape_book_profile_page(@book_list[input -1]) if @book_list[input -1].summary == nil # Gets the book's summary information
+    # Scrapes to get the book's summary information if the Book object doesn't already include summary information
+    Scraper.scrape_book_profile_page(@book_list[input -1]) if @book_list[input -1].summary == nil
     puts ""
-    puts @book_list[input -1].summary
+    puts @book_list[input -1].summary # Prints the book's summary information to the screen
     puts ""
     puts "Would you like to read a summary of another one of the books from your search results? (y/n)"
     input = gets.strip
@@ -86,22 +92,7 @@ class CLI # This class runs the command-line interface for the gem
     end
     if input == "y" # Reprints list of books if user wants to read another summary from that list
       puts ""
-      @book_list.each_with_index do |book, index|
-        if book.authors.length == 1 # Formatting for book with one author
-          puts "#{index + 1}. #{book.title} by #{book.authors[0]}"
-        elsif book.authors.length == 2 # Formatting for book with two authors
-          puts "#{index + 1}. #{book.title} by #{book.authors[0]} and #{book.authors[1]}"
-        else # Formatting for book with three or more authors
-          authors = ""
-          i = 0
-          while i < book.authors.length - 2
-            authors += "#{book.authors[i]}, "
-            i += 1
-          end
-          authors += "#{book.authors[book.authors.length - 2]} and #{book.authors[book.authors.length - 1]}"
-          puts "#{index + 1}. #{book.title} by #{authors}"
-        end
-      end
+      list_books # Reruns list_books method to reprint the list of books
       puts ""
       show_summary # Reruns the show_summary method if the user wants to read another summary from the current list of books
     elsif input == "n" # Allows user to conduct another search for books if no summary is wanted from the current list
